bdd508d03e289905e90b6a5dc51091ea55457f19
heyavinash-in <avifact333@gmail.com>
2026-08-22 22:51:16 +0530

    Day 01: sum of numbers from 1 to n

---
+++ adder.java
@@ -0,0 +1,12 @@
+public class adder {
+    public static void main(String[] args) {
+        int sum =0;
+        for(int i = 0; i<= 100; i++){
+            sum+=i;
+        }
+            System.out.println(sum);
+           
+
+        
+    }
+}
