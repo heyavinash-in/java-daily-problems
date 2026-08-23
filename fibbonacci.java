02545da6e16149e8956c14c8c315b52e89d28f03
heyavinash-in <avifact333@gmail.com>
2026-08-23 22:19:23 +0530

    Day 02: Fibonacci series

---
+++ fibbonacci.java
@@ -0,0 +1,13 @@
+public class fibbonacci {
+    public static void main(String[] args) {
+        int a = 0, b = 1;
+
+        for(int i= 0; i<=10; i++ ){
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
