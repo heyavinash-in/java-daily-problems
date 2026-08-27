c1bbee6f025d0e2dfdf67caa9a79f339e4fd99d6
heyavinash-in <avifact333@gmail.com>
2026-08-27 23:05:49 +0530

    Day 06 :inverted repeated alphabet patern printer

---
+++ invertedalphabetrepeated.java
@@ -0,0 +1,9 @@
+public class invertedalphabetrepeated {
+    public static void main(String[] args) {
+        for(char i = 'E'; i>='A'; i--){
+            for(char j = 'A'; j<=i; j++){
+                System.out.print(i+ " ");
+            }System.out.println();
+        }
+    }
+}
