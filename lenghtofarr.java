b9003ef73605ca1ad1f9ef0d0a1e0b6752661bdb
heyavinash-in <avifact333@gmail.com>
2026-08-28 23:18:01 +0530

    Day 07: Length of an array

---
+++ lenghtofarr.java
@@ -0,0 +1,10 @@
+public class lenghtofarr {
+    
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Length = " + arr.length);
+    }
+}
+
