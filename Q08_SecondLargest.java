7384de0bd482cf300ad1502b5a67907e663232ae
heyavinash-in <avifact333@gmail.com>
2026-09-05 23:02:43 +0530

    Day 15 : Second largest in a 1Darray

---
+++ Q08_SecondLargest.java
@@ -0,0 +1,22 @@
+public class Q08_SecondLargest {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 45, 30, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + second);
+    }
+}
