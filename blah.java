ca445ad56863952bb4b46fec25c4cb8e7e82d72c
heyavinash-in <avifact333@gmail.com>
2026-09-11 23:25:53 +0530

    Day 19: name age collage

---
+++ blah.java
@@ -0,0 +1,12 @@
+public class blah {
+    
+    public static void main(String[] args) {
+        String name = "Avinash";
+        int age = 20;
+        String college = "GITAM";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("College: " + college);
+    }
+}
