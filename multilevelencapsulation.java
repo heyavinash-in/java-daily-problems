4a7253b220cf43aa5490c2ed857e4dcc5f46d39e
heyavinash-in <avifact333@gmail.com>
2026-09-23 21:11:06 +0530

    Day 28: MultiLevel Inheritance in java

---
+++ multilevelencapsulation.java
@@ -0,0 +1,27 @@
+class Animal {
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
+class Puppy extends Dog {
+    void play() {
+        System.out.println("Playing");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+
+        p.eat();
+        p.bark();
+        p.play();
+    }
+}
